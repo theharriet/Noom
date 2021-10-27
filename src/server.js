deaff04cabd3037e57f53661950a4426726f2477
theharriet <goand123@gmail.com>
2021-10-27 14:58:03 +0900

    http vs webSocket

--- src/server.js
+++ src/server.js
@@ -5,25 +5,25 @@ const app = express();
 app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-
-//console.log("Hello");
-//여기서 express로 할일은 views를 정해주고 render해주는것
-// 나머지는 websocket에서 실시간으로 일어날거임
-app.get("/", (req, res) => res.render("home")); //여기서 필요한 유일한 route
+app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/")); 
-//catchall url : 유져가 어떤 url로 이동하던지 홈으로 돌려보냄. 다른 url안쓸꺼니까 only home
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
 
-//line5: pug로 view enging을 설정하고
-//line6: express에 template이 어디있는지 지정
-//line7: public url을 생성해서 유저에게 파일을 공유해주고
-//line13: home.pug를 render해주는 route handler를 만들었음
+//실시간 채팅 프로그램
+// 1. 메세지 주고 받기
+// 2. 닉네임 설정
+// 3. 방만들기
+// 4. event : %%님이 입장하였습니다 or 실시간으로 채팅방에 몇명있는지
 
-//app.js를(user에게 보여지는 화면-Frontend) 저장할때마다 nodemon이 restart되고 있어
-// 그치만 이걸 원하는게 아냐
-// views나 server.js를(or 모든 자바스크립트파일들이) 수정될때만 nodemon이 재시작하길 바람
-//(Frontend 자바스크립트를 수정할때는 제외하고)
-// --> nodemon.json가서 폴더하나를 무시하도록 설정
-// "ignore"를 입력하고 src/public 폴더안에 모든 파일들 무시하도록.
+//http 프로토콜 : 브라우저 request <-> 서버 response. https://nomadcoder.co
+//              서버가 response후에는 네가 누구인지 잊어버림 -> stateless : 그래서 req할때 cookie를 같이 보내야해 그럼 서버가 일치하는 profile로 응답
+//              서버는 오직 request받을때만 응답해줌
+//              real-time으로 일어나지 않음
+
+//webSocket 프로토콜 : (ws://nomadcoder.co) 브라우저에 webSocket API가 내장되어있음
+//      connect - like handshake. 브라우저가 서버로 websocket request보내면 서버는 받거나 거절
+//              서버가 받으면 the connection is established
+//  서버가 연결되어있기 때문에 네가 누구인지 기억할수있음. 원한다면 서버가 유저에게 메세지 보낼수있음
+//  the server doesn't have to wait for a request. bi-directional connection 
