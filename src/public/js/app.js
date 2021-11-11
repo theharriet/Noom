eeb53c462ef23603359de6ee50cf66c41edb0592
theharriet <goand123@gmail.com>
2021-11-11 16:51:49 +0900

    getUserMedia

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,6 +2,24 @@ const socket = io();
 
 const myFace = document.getElementById("myFace");
 // stream : 비디오와 오디오가 결합된것 
+let myStream;
+
+async function getMedia(){
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: true,
+        });
+        console.log(myStream);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+getMedia();
+//navigator.mediaDevices.getUserMedia(constraints)는 유저의 유저미디어 string을 줄거임
+// constraints - 우리가 얻고싶은것
+//https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia
 
 //#3.0 2:50
 
